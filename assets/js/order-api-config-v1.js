93ca5599c0b78b2079e088da4d6d7c8cfaa108b2
NoshiBakery <mremadcraftplugins@gmail.com>
2026-08-23 21:18:29 +0300

    تغديل

--- assets/js/order-api-config-v1.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-// رابط Cloudflare Worker الخاص بطلبات نوشي. لا تضع Telegram Bot Token هنا.
-window.NOSHI_ORDER_API_URL = "https://noshi-order-api.mremadcraftplugins.workers.dev/api/orders";
